cd13daea9d9e874281aa1a9b36a6f07be252ea6d
Bo Zhao <jakobzhao@users.noreply.github.com>
2021-02-25 12:37:26 -0800

    Update iot.sh

--- weeks/week08/rpi-iot/iot.sh
+++ weeks/week08/rpi-iot/iot.sh
@@ -3,4 +3,4 @@ sudo git pull https://[github-username]:[github-password]@github.com/jakobzhao/r
 sudo /usr/bin/python3 /home/pi/rpi-iot/01_env_sensor.py
 sudo git add .
 sudo git commit -a -m 'Env Bot'
-sudo git push https://hgislab:SmithHall406A@github.com/jakobzhao/rpi-iot.git
+sudo git push https://[github-username]:[github-password]@github.com/jakobzhao/rpi-iot.git
